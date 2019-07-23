91b0bd12619caafd09b4855994f1a89755d22edf
Ridho Putra <ridho.putra@xtremax.com>
2019-07-23 11:19:17 +0700

    regression

---
+++ regressions/index.js
@@ -0,0 +1,21 @@
+require('@tensorflow/tfjs-node')
+const tf = require('@tensorflow/tfjs')
+const loadCSV = require('./load-csv')
+const LinearRegression = require('./linear-regression')
+
+let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ['horsepower', 'weight', 'displacement'],
+  labelColumns: ['mpg']
+})
+
+const regression = new LinearRegression(features, labels, {
+  learningRate: .1,
+  iterations: 100
+})
+
+regression.train()
+const r2 = regression.test(testFeatures, testLabels)
+
+console.log('RS is', r2)
---
+++ regressions/linear-regression.js
@@ -0,0 +1,119 @@
+const tf = require('@tensorflow/tfjs')
+const _ = require('lodash')
+
+class LinearRegression {
+  constructor(features, labels, options) {
+    this.features = this.processFeatures(features)
+    this.labels = tf.tensor(labels)
+    this.mseHistory = []
+
+    this.options = Object.assign({
+      learningRate: 0.1,
+      iterations: 1000
+    }, options)
+
+    this.weights = tf.zeros([this.features.shape[1], 1])
+  }
+
+  gradientDescent() {
+    const currentGuesses = this.features.matMul(this.weights)
+    const differences = currentGuesses.sub(this.labels)
+
+    const slopes = this.features
+      .transpose()
+      .matMul(differences)
+      .div(this.features.shape[0])
+
+    this.weights = this.weights.sub(slopes.mul(this.options.learningRate))
+  }
+
+  train() {
+    for (let i = 0; i < this.options.iterations; i++) {
+      this.gradientDescent()
+      this.recordMSE()
+      this.updateLearningRate()
+    }
+  }
+
+  test(testFeatures, testLabels) {
+    testFeatures = this.processFeatures(testFeatures)
+    testLabels = tf.tensor(testLabels)
+
+    const predictions = testFeatures.matMul(this.weights)
+
+    const res = testLabels.sub(predictions)
+      .pow(2)
+      .sum()
+      .arraySync()
+
+    const tot = testLabels.sub(testLabels.mean())
+      .pow(2)
+      .sum()
+      .arraySync()
+
+    return 1 - res / tot
+  }
+
+  processFeatures(features) {
+    features = tf.tensor(features)
+
+    if (this.mean && this.variance) {
+      features = features.sub(this.mean).div(this.variance.pow(.5))
+    } else {
+      features = this.standardize(features)
+    }
+
+    features = tf.ones([features.shape[0], 1]).concat(features, 1)
+
+    return features
+  }
+
+  standardize(features) {
+    const {
+      mean,
+      variance
+    } = tf.moments(features, 0)
+
+    this.mean = mean;
+    this.variance = variance
+
+    return features.sub(mean).div(variance.pow(.5))
+  }
+
+  recordMSE() {
+    const mse = this.features
+      .matMul(this.weights)
+      .sub(this.labels)
+      .pow(2)
+      .sum()
+      .div(this.features.shape[0])
+      .arraySync()
+
+    this.mseHistory.unshift(mse)
+  }
+
+  updateLearningRate() {
+    if (this.mseHistory.length < 2) {
+      return
+    }
+
+    if (this.mseHistory[0] > this.mseHistory[1]) {
+      this.options.learningRate /= 2
+    } else {
+      this.options.learningRate *= 1.05
+    }
+  }
+
+  // gradientDescent() {
+  //   const currentGuessesForMPG = this.features.map(row => this.m * row[0] + this.b)
+
+  //   const bSlope = _.sum(currentGuessesForMPG.map((guess, i) => guess - this.labels[i][0])) * 2 / this.features.length
+
+  //   const mSlope = _.sum(currentGuessesForMPG.map((guess, i) => -1 * this.features[i][0] * (this.labels[i][0] - guess))) * 2 / this.features.length
+
+  //   this.m = this.m - mSlope * this.options.learningRate
+  //   this.b = this.b - bSlope * this.options.learningRate
+  // }
+}
+
+module.exports = LinearRegression
